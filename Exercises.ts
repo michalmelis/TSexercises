4bf401f58ae8316620a086aee4f49b5a282dace1
michalmelis <michalmelis.sk@gmail.com>
2020-10-20 22:29:58 +0200

    Done.

--- Exercises.ts
+++ Exercises.ts
@@ -1,33 +1,45 @@
 console.log(totalDistance(0.2,0.4,100))
 console.log(equal(1,1,1))
 console.log(isTriangle(1,2,3))
-
+console.log(multiplyByLength([1,2,3,0]))
 
 
 
 function totalDistance(heightOfstep:number, lenghtOfstep:number,towerHeight:number):number
 {
     let totalDistance = (towerHeight/heightOfstep) * (lenghtOfstep + heightOfstep);
-     return totalDistance
+          return totalDistance;
 }
 
 function equal(a:number, b:number, c:number)
 {
     if  (a == b && b==c)
     {
-        return  3;
+     return  3;
     }
-    else if ( a == b || b==c || a==c)
-    {
-    return  2;
-    }
+        else if ( a == b || b==c || a==c)
+       {
+         return  2;
+       }
     else
-        {
-          return 0;
-        }
+    {
+     return 0;
+    }
 }
 
 function isTriangle(a:number, b:number, c:number):boolean
 {
     return (a+b)>c;
 }
+   
+function multiplyByLength(array) 
+{
+   for (let i = 0; i < array.length; i++) 
+   {
+        array[i] *= array.length;
+   }
+ return array;
+}
+
+ 
+   
--- dist/Exercises.js
+++ dist/Exercises.js
@@ -1,6 +1,7 @@
 console.log(totalDistance(0.2, 0.4, 100));
 console.log(equal(1, 1, 1));
 console.log(isTriangle(1, 2, 3));
+console.log(multiplyByLength([1, 2, 3, 0]));
 function totalDistance(heightOfstep, lenghtOfstep, towerHeight) {
     let totalDistance = (towerHeight / heightOfstep) * (lenghtOfstep + heightOfstep);
     return totalDistance;
@@ -18,5 +19,11 @@ function equal(a, b, c) {
 }
 function isTriangle(a, b, c) {
     return (a + b) > c;
+}
+function multiplyByLength(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] *= array.length;
+    }
+    return array;
 }
 //# sourceMappingURL=Exercises.js.map
