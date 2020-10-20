5c7e49263beaa55c03af30f3551bd1998337dac1
michalmelis <michalmelis.sk@gmail.com>
2020-10-20 20:52:08 +0200

    Done.

--- Exercises.ts
+++ Exercises.ts
@@ -1,7 +1,27 @@
 console.log(totalDistance(0.2,0.4,100))
+console.log(equal(1,1,1))
+
+
+
 
 function totalDistance(heightOfstep:number, lenghtOfstep:number,towerHeight:number):number
 {
     let totalDistance = (towerHeight/heightOfstep) * (lenghtOfstep + heightOfstep);
      return totalDistance
+}
+
+function equal(a:number, b:number, c:number)
+{
+    if  (a == b && b==c)
+    {
+        return  3;
+    }
+    else if ( a == b || b==c || a==c)
+    {
+    return  2;
+    }
+    else
+        {
+          return 0;
+        }
 }
--- dist/Exercises.js
+++ dist/Exercises.js
@@ -1,6 +1,18 @@
 console.log(totalDistance(0.2, 0.4, 100));
+console.log(equal(1, 1, 1));
 function totalDistance(heightOfstep, lenghtOfstep, towerHeight) {
     let totalDistance = (towerHeight / heightOfstep) * (lenghtOfstep + heightOfstep);
     return totalDistance;
+}
+function equal(a, b, c) {
+    if (a == b && b == c) {
+        return 3;
+    }
+    else if (a == b || b == c || a == c) {
+        return 2;
+    }
+    else {
+        return 0;
+    }
 }
 //# sourceMappingURL=Exercises.js.map
